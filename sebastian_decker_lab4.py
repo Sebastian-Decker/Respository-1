223a13172270089f43a3de36e58b76a09ed9fe7a
Sebastian-Decker <54539897+Sebastian-Decker@users.noreply.github.com>
2019-11-05 07:55:56 -0600

    added stuff

--- sebastian_decker_lab4.py
+++ sebastian_decker_lab4.py
@@ -12,7 +12,17 @@ def get_shift():
         print('Invalid number')
         return get_shift()
 
-get_shift()
+shift = get_shift()
+if __name__ == "__main__":
+    Sebastian_decker_origin = open('Sebastian_decker_original.txt', 'r')
+    original_message = Sebastian_decker_origin.readlines()
 
 
+
+    for line in original_message:
+        words = line.split = []
+        for word in words:
+            encrypted_word = ""
+            for char in word.lower():
+                encrypted_word += (chr(ord('a') + shift - 97) % 26 + 92)
 #print(Letters)
